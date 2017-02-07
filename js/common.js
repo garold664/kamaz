0996af41edb403881906f9629d0d8913956d841c
garold664 <garold664@gmail.com>
2017-02-07 09:09:08 +0300

    заменил фон в хедере на 960пх

--- js/common.js
+++ js/common.js
@@ -47,7 +47,10 @@ function setDropdownToggle(toggleBtn) {
   closeBtn.on("click", function(event) {
     event.preventDefault();
     console.log(event.target);
-    dropdown.toggleClass(dropdownClassname + "--closed");
+    var dropdownEl = ($(this).parent().hasClass(dropdownClassname)) ?
+                     ($(this).parent()) :
+                     ($(this).parent().find(dropdown));
+    dropdownEl.toggleClass(dropdownClassname + "--closed");
     $(this).toggleClass(toggleBtn + "--opened");
   });
 }
@@ -58,3 +61,15 @@ setDropdownToggle("page-header__nav-btn");
 setDropdownToggle("page-header__search-btn");
 setDropdownToggle("page-footer__nav-link");
 setDropdownToggle("page-header__flags-btn");
+
+var nextBtn = $(".main__history-nav-btn--next");
+var historyList = $(".main__history-nav-list");
+var historyItem = $(".main__history-nav-item");
+
+nextBtn.on("click", function(event) {
+  historyList.animate({
+    left: "-=150"
+  });
+  console.log(historyList.css.left);
+  console.log(historyItem.css.width);
+});
